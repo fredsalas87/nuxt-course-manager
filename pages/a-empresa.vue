e8aea0d9b72fe6f39b0df59e81c0e9b6fd2cf4eb
fredsalas87 <fredsalas87@gmail.com>
2023-03-28 20:03:50 -0300

    Criando pages: empresa e serviços

---
+++ pages/a-empresa.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-app-bar elevation="4" class="mb-5">
+      <nuxt-link :to="{ name: 'a-empresa'}">A empresa</nuxt-link>
+      <nuxt-link :to="{ name: 'servicos'}">Serviços</nuxt-link>
+    </v-app-bar>
+    <h1>A empresa</h1>
+    <p>lipsum</p>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/servicos.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-app-bar elevation="4" class="mb-5">
+      <nuxt-link :to="{ name: 'a-empresa'}" class="mr-5">A empresa</nuxt-link>
+      <nuxt-link :to="{ name: 'servicos'}">Serviços</nuxt-link>
+    </v-app-bar>
+    <h1>Serviços</h1>
+    <p>lipsum</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
